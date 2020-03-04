b14a9711b5fc1af31b53b2426c67c981b7a3872d
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-03-04 17:15:08 -0500

    Taller Finalizado

--- luminosity/views.py
+++ luminosity/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+
 from .models import Luminosity
 from .serializers import LuminositySerializer
 
